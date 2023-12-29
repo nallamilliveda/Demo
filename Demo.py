864658ec81eb72a126974e6e88b6e79a182bdbab
nallamilliveda <154418958+nallamilliveda@users.noreply.github.com>
2023-12-29 12:13:20 +0530

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -2,13 +2,13 @@ import streamlit as st
 st.set_page_config(page_title = 'cats')
 st.markdown("## Types of cats")
 col1,col2 = st.columns(2)
- with col1:
+with col1:
   st.subheader("-persian cat")
   st.image("https://th.bing.com/th/id/OIP.eZk5ARN29UZ1pOgVsmSixAHaFj?w=233&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7") 
   st.write("persion cats are cute")
- with col2:
+with col2:
   st.subheader("-White cat")
   st.image("./giphy.gif")
   st.write("white cats are cute")
-st.image("./white cat.jpg")  
+  st.image("./white cat.jpg")  
  
