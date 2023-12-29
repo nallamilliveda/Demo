2a79f3ea4002d64aefbda0153e5ea0e800e1273e
nallamilliveda <154418958+nallamilliveda@users.noreply.github.com>
2023-12-29 11:10:44 +0530

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -1,7 +1,9 @@
 import streamlit as st
+from PIL import image
 st.set_page_config(page_title = 'cats')
 st.markdown("## Types of cats")
 st.write("-persian cat")
 st.image("C:\movie\persian cat.jpg")
+img = image.open("C:\movie\persian cat.jpg")
 st.write("-White cat")
 st.image("C:\movie\white cat.jpg")
